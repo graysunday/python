69a668fb8c1cf9375913da220a49e0f2f21c431d
graysunday <graysunday77@gmail.com>
2023-05-28 23:20:16 +0900

    예제소스

---
+++ 02B-turtle.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+t.shape("turtle")
+
+#삼각형 그리기
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+#사각형 그리기
+t.forward(100)
+t.left(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+#원 그리기
+t.circle(50)
