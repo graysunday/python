6332d3bacce334bd621b97ba688c8f2177595599
graysunday <graysunday77@gmail.com>
2023-06-01 20:17:57 +0900

    늦게 commit

---
+++ 04C-turtle.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+t.shape("turtle")
+
+#삼각형 그리기
+for x in range(3):
+    t.fd(100)
+    t.lt(120)
+
+#사각형 그리기
+for x in range(4):
+    t.fd(100)
+    t.lt(90)
+
+#원 그리기
+t.circle(50)
+t.bye()
